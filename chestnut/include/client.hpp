1cb214f0587cf178886cb6b0f9f1b0de4b1453bc
andreymlv <ndrmlv@ya.ru>
2024-03-13 14:55:08 +0300

    upd

--- chestnut/include/client.hpp
+++ chestnut/include/client.hpp
@@ -12,48 +12,22 @@
 
 #include "sockaddr.hpp"
 
-class AudioInfo: public QIODevice {
-  Q_OBJECT
-
- public:
-  AudioInfo(QAudioFormat const& format);
-
-  void start();
-  void stop();
-
-  qreal level() const {
-    return m_level;
-  }
-
-  qint64 readData(char* data, qint64 maxlen) override;
-  qint64 writeData(char const* data, qint64 len) override;
-
-  qreal calculateLevel(char const* data, qint64 len) const;
-
- signals:
-  void levelChanged(qreal level);
-
- private:
-  QAudioFormat const m_format;
-  qreal m_level = 0.0;  // 0.0 <= m_level <= 1.0
-};
-
 class Client: public QObject {
   Q_OBJECT
 
   struct M {
     SockAddr addr = SockAddr {QHostAddress::LocalHost, 0};
     QUdpSocket* socket = nullptr;
+
     QAudioSink* audioSink = nullptr;
     QIODevice* sink = nullptr;
     QTimer* pushTimer = nullptr;
-
-    QMediaDevices* m_devices = nullptr;
-    AudioInfo* audioInfo = nullptr;
-    QAudioSource* audioInput = nullptr;
-
-    OpusEncoder* encoder = nullptr;
     OpusDecoder* decoder = nullptr;
+
+    QAudioSource* audioInput = nullptr;
+    QIODevice* source = nullptr;
+    QTimer* pullTimer = nullptr;
+    OpusEncoder* encoder = nullptr;
   } m;
 
   Client(M const& m, QObject* parent);
@@ -76,6 +50,6 @@ class Client: public QObject {
   create(SockAddr const& addr, QAudioDevice const& deviceInfo, QObject* parent);
 
  private slots:
-  void record(QIODevice* io);
+  void record();
   void playback();
 };
--- chestnut/src/client.cpp
+++ chestnut/src/client.cpp
@@ -1,80 +1,17 @@
 #include "client.hpp"
 
-AudioInfo::AudioInfo(QAudioFormat const& format) : m_format(format) {}
-
-void AudioInfo::start() {
-  open(QIODevice::WriteOnly);
-}
-
-void AudioInfo::stop() {
-  close();
-}
-
-qint64 AudioInfo::readData(char* /* data */, qint64 /* maxlen */) {
-  return 0;
-}
-
-qreal AudioInfo::calculateLevel(char const* data, qint64 len) const {
-  int const channelBytes = m_format.bytesPerSample();
-  int const sampleBytes = m_format.bytesPerFrame();
-  int const numSamples = len / sampleBytes;
-
-  float maxValue = 0;
-  auto* ptr = reinterpret_cast<unsigned char const*>(data);
-
-  for (int i = 0; i < numSamples; ++i) {
-    for (int j = 0; j < m_format.channelCount(); ++j) {
-      float value = m_format.normalizedSampleValue(ptr);
-
-      maxValue = qMax(value, maxValue);
-      ptr += channelBytes;
-    }
-  }
-  return maxValue;
-}
-
-qint64 AudioInfo::writeData(char const* data, qint64 len) {
-  m_level = calculateLevel(data, len);
-
-  emit levelChanged(m_level);
-
-  return len;
-}
-
 Client::Client(M const& m, QObject* parent) : QObject(parent), m(std::move(m)) {
-  auto* io = m.audioInput->start();
-  if (!io)
-    return;
-  connect(io, &QIODevice::readyRead, [this, &io, &m] {
-    static qint64 const BufferSize = 4096;
-    qint64 const len = qMin(m.audioInput->bytesAvailable(), BufferSize);
-    qDebug() << len;
-
-    QByteArray buffer(len, 0);
-    qint64 l = io->read(buffer.data(), len);
-
-    if (m.socket->write(buffer) < 0) {
-      qDebug() << "Server is shutdown?";
-      stop();
-      return;
-    }
-  });
-
-  /* connect(m.pullTimer, &QTimer::timeout, this, &Client::record); */
+  connect(m.pullTimer, &QTimer::timeout, this, &Client::record);
   connect(m.pushTimer, &QTimer::timeout, this, &Client::playback);
 }
 
 Client::~Client() {
-  m.pushTimer->stop();
+  stop();
   if (m.socket) {
     if (m.socket->isOpen()) {
       m.socket->close();
     }
     delete m.socket;
-  }
-  if (m.audioSink) {
-    m.audioSink->stop();
-    delete m.audioSink;
   }
   opus_encoder_destroy(m.encoder);
   opus_decoder_destroy(m.decoder);
@@ -90,16 +27,17 @@ Client Client::create(
   socket->connectToHost(addr.ip, addr.port);
 
   QAudioFormat format;
-  format.setSampleRate(48000);
+  format.setSampleRate(16000);
   format.setChannelCount(1);
   format.setSampleFormat(QAudioFormat::Int16);
-  auto* audioInfo = new AudioInfo(format);
+
   auto* audioInput = new QAudioSource(deviceInfo, format);
+  audioInput->setBufferSize(4096);
   auto* audioSink = new QAudioSink(format, parent);
 
   int opusError = OPUS_OK;
   auto* encoder = opus_encoder_create(
-    16000,
+    format.sampleRate(),
     format.channelCount(),
     OPUS_APPLICATION_VOIP,
     &opusError
@@ -113,26 +51,41 @@ Client Client::create(
     qWarning() << "Failed to create Opus encoder.";
   }
 
-  audioInfo->start();
-
+  auto* source = audioInput->start();
   auto* sink = audioSink->start();
   return Client(
     M {
       .addr = addr,
       .socket = socket,
+
       .audioSink = audioSink,
       .sink = sink,
       .pushTimer = new QTimer,
-      .audioInfo = audioInfo,
-      .audioInput = audioInput,
-      .encoder = encoder,
       .decoder = decoder,
+
+      .audioInput = audioInput,
+      .source = source,
+      .pullTimer = new QTimer,
+      .encoder = encoder,
+
     },
     parent
   );
 }
 
-void Client::record(QIODevice* io) {}
+void Client::record() {
+  static qint64 const BufferSize = 4096;
+  qint64 const len = qMin(m.audioInput->bytesAvailable(), BufferSize);
+
+  QByteArray buffer(len, 0);
+  qint64 l = m.source->read(buffer.data(), len);
+
+  if (m.socket->write(buffer) < 0) {
+    qDebug() << "Server is shutdown?";
+    stop();
+    return;
+  }
+}
 
 void Client::playback() {
   auto bytes = m.audioSink->bytesFree();
@@ -144,13 +97,13 @@ void Client::playback() {
 }
 
 void Client::start() {
-  /* m.pullTimer->start(); */
+  m.pullTimer->start();
   m.pushTimer->start();
 }
 
 void Client::stop() {
-  /* m.pullTimer->stop(); */
+  m.pullTimer->stop();
   m.pushTimer->stop();
   m.audioSink->stop();
-  /* m.audioSource->stop(); */
+  m.audioInput->stop();
 }
